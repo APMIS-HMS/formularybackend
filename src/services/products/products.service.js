2fc394bbc5b251d2d61aa77b3bf4c983ea8c9bb6
starday01 <sunday@apmis.ng>
2018-04-27 11:33:49 +0100

    implement search products

--- src/services/products/products.service.js
+++ src/services/products/products.service.js
@@ -3,21 +3,21 @@ const createService = require('feathers-mongoose');
 const createModel = require('../../models/products.model');
 const hooks = require('./products.hooks');
 
-module.exports = function (app) {
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+module.exports = function(app) {
+    const Model = createModel(app);
+    const paginate = app.get('paginate');
 
-  const options = {
-    name: 'products',
-    Model,
-    paginate
-  };
+    const options = {
+        name: 'products',
+        Model,
+        paginate
+    };
 
-  // Initialize our service with any options it requires
-  app.use('/products', createService(options));
+    // Initialize our service with any options it requires
+    app.use('/products', createService(options));
 
-  // Get our initialized service so that we can register hooks and filters
-  const service = app.service('products');
+    // Get our initialized service so that we can register hooks and filters
+    const service = app.service('products');
 
-  service.hooks(hooks);
+    service.hooks(hooks);
 };
