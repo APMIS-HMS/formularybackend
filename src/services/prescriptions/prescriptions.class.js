f3944efef74f8673c1e838db2aef2a711377407d
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-12-29 02:45:38 +0100

    Updates to prescription class

--- src/services/prescriptions/prescriptions.class.js
+++ src/services/prescriptions/prescriptions.class.js
@@ -1,140 +1,148 @@
 /* eslint-disable no-unused-vars */
 const jsend = require('jsend');
 class Service {
-  constructor(options) {
-    this.options = options || {};
-  }
-  async find(params) {
-    if (params.query.search == undefined || params.query.search.length >= 4) {
-      const conditions = ['RXNORM', 'RXNORM NG'];
-      const consoService = this.app.service('rxnconso');
-      const awaitConsoService = await consoService.find({
-        query: {
-          STR: {
-            $regex: params.query.search,
-            '$options': 'i'
-          },
-          SAB: {
-            $in: conditions
-          },
-          'TTY': 'SCD',
-          $limit: params.query.$limit ? params.query.$limit : 10,
-          $skip: params.query.$skip ? params.query.$skip : 0
-        }
-      });
-      const data = awaitConsoService.data.map(this.reFactorPrescriptionData);
-      awaitConsoService.data = data;
-      return jsend.success(awaitConsoService);
-    } else {
-      return jsend.fail({
-        validation: ['search is required and must be more than 3 characters!!!']
-      });
-    }
-  }
+	constructor(options) {
+		this.options = options || {};
+	}
+	async find(params) {
+		if (params.query.search == undefined || params.query.search.length >= 4) {
+			let regexList = [];
+			params.query.search.split(',').forEach((value) => {
+				regexList.push({
+					STR: {
+						$regex: value,
+						$options: 'gmi'
+					}
+				});
+			});
 
-  reFactorPrescriptionData(data) {
-    return {
-      id: data._id,
-      name: data.STR,
-      code: data.RXCUI
-    };
-  }
+			const conditions = [ 'RXNORM', 'RXNORM NG' ];
+			const consoService = this.app.service('rxnconso');
 
-  async get(id, params) {
-    const relService = this.app.service('rxnrel');
-    const consoService = this.app.service('rxnconso');
-    const conditions = ['RXNORM', 'RXNORM NG'];
+			var regex = [];
+			for (var i = 0; i < params.query.search.split(',').length; i++) {
+				regex[i] = new RegExp(params.query.search.split(',')[i], 'gmi');
+			}
 
-    const awaitSelectedConsoService = await consoService.find({
-      query: {
-        'RXCUI': id,
-        SAB: {
-          $in: conditions
-        },
-        'TTY': 'SCD'
-      }
-    });
+			const awaitConsoService = await consoService.find({
+				query: {
+					$and: [ ...regexList ],
+					SAB: {
+						$in: conditions
+					},
+					TTY: 'SCD',
+					$limit: params.query.$limit ? params.query.$limit : 20,
+					$skip: params.query.$skip ? params.query.$skip : 0
+				}
+			});
+			const data = awaitConsoService.data.map(this.reFactorPrescriptionData);
+			awaitConsoService.data = data;
+			return jsend.success(awaitConsoService);
+		} else {
+			return jsend.fail({
+				validation: [ 'search is required and must be more than 3 characters!!!' ]
+			});
+		}
+	}
 
-    if (awaitSelectedConsoService.data.length > 0) {
-      const awaitDoseForm = await relService.find({
-        query: {
-          'RXCUI1': id,
-          'RELA': 'dose_form_of'
-        }
-      });
-      if (awaitDoseForm.data.length > 0) {
-        const rela = awaitDoseForm.data[0];
-        const relaCUI = rela.RXCUI2;
+	reFactorPrescriptionData(data) {
+		return {
+			id: data._id,
+			name: data.STR,
+			code: data.RXCUI
+		};
+	}
 
+	async get(id, params) {
+		const relService = this.app.service('rxnrel');
+		const consoService = this.app.service('rxnconso');
+		const conditions = [ 'RXNORM', 'RXNORM NG' ];
 
-        const awaitConsoService = await consoService.find({
-          query: {
-            'RXCUI': relaCUI,
-            SAB: {
-              $in: conditions
-            },
-          }
-        });
-        const sub = awaitConsoService.data.map(this.reFactorPrescriptionData);
-        const main = awaitSelectedConsoService.data.map(this.reFactorPrescriptionData);
-        awaitSelectedConsoService.data = main;
-        awaitSelectedConsoService.data.forEach(main => {
-          main.dose_form = sub;
-        });
-        return jsend.success(awaitSelectedConsoService);
-      } else {
-        return jsend.fail({
-          validation: ['supplied code does not exist on APMIS FORMULARY']
-        });
-      }
-    } else {
-      return jsend.fail({
-        validation: ['supplied code does not exist on APMIS FORMULARY']
-      });
-    }
+		const awaitSelectedConsoService = await consoService.find({
+			query: {
+				RXCUI: id,
+				SAB: {
+					$in: conditions
+				},
+				TTY: 'SCD'
+			}
+		});
 
+		if (awaitSelectedConsoService.data.length > 0) {
+			const awaitDoseForm = await relService.find({
+				query: {
+					RXCUI1: id,
+					RELA: 'dose_form_of'
+				}
+			});
+			if (awaitDoseForm.data.length > 0) {
+				const rela = awaitDoseForm.data[0];
+				const relaCUI = rela.RXCUI2;
 
+				const awaitConsoService = await consoService.find({
+					query: {
+						RXCUI: relaCUI,
+						SAB: {
+							$in: conditions
+						}
+					}
+				});
+				const sub = awaitConsoService.data.map(this.reFactorPrescriptionData);
+				const main = awaitSelectedConsoService.data.map(this.reFactorPrescriptionData);
+				awaitSelectedConsoService.data = main;
+				awaitSelectedConsoService.data.forEach((main) => {
+					main.dose_form = sub;
+				});
+				return jsend.success(awaitSelectedConsoService);
+			} else {
+				return jsend.fail({
+					validation: [ 'supplied code does not exist on APMIS FORMULARY' ]
+				});
+			}
+		} else {
+			return jsend.fail({
+				validation: [ 'supplied code does not exist on APMIS FORMULARY' ]
+			});
+		}
+	}
+	reFactorPrescriptionWithDoseFormData(conso) {
+		let main = {
+			id: conso._id,
+			name: conso.STR,
+			code: conso.RXCUI,
+			dose_form: null
+		};
+		return main;
+	}
+	create(data, params) {
+		if (Array.isArray(data)) {
+			return Promise.all(data.map((current) => this.create(current)));
+		}
 
-  }
-  reFactorPrescriptionWithDoseFormData(conso) {
-    let main = {
-      id: conso._id,
-      name: conso.STR,
-      code: conso.RXCUI,
-      dose_form: null
-    };
-    return main;
-  }
-  create(data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current)));
-    }
+		return Promise.resolve(data);
+	}
 
-    return Promise.resolve(data);
-  }
+	update(id, data, params) {
+		return Promise.resolve(data);
+	}
 
-  update(id, data, params) {
-    return Promise.resolve(data);
-  }
+	patch(id, data, params) {
+		return Promise.resolve(data);
+	}
 
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
+	remove(id, params) {
+		return Promise.resolve({
+			id
+		});
+	}
 
-  remove(id, params) {
-    return Promise.resolve({
-      id
-    });
-  }
-
-  setup(app) {
-    this.app = app;
-  }
-
+	setup(app) {
+		this.app = app;
+	}
 }
 
-module.exports = function (options) {
-  return new Service(options);
+module.exports = function(options) {
+	return new Service(options);
 };
 
 module.exports.Service = Service;
